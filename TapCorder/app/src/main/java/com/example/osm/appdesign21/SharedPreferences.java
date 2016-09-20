b7378537cca425a1613e89d420c50c5645821903
이명호 <12audgh@naver.com>
2016-09-20 20:17:34 +0900

    [명호]연락처를 저장해두기 위한 SharedPreferences Class 구현

---
+++ TapCorder/app/src/main/java/com/example/osm/appdesign21/SharedPreferences.java
@@ -0,0 +1,47 @@
+package com.example.osm.appdesign21;
+
+import android.app.Activity;
+import android.content.Context;
+
+/**
+ * Created by mh on 2016-09-20.
+ */
+public class SharedPreferences {
+
+    static Context mContext;
+
+    public SharedPreferences(Context c){
+        mContext = c;
+    }
+
+    /**
+     * 값 넣기
+     * @param paramKey (값이 없을경우 default: ""공백 리턴)
+     * @param value 저장할 값
+     * @param prefName 파일 이름
+     */
+    public void putValue(String paramKey, String value, String prefName){
+        android.content.SharedPreferences pref = mContext.getSharedPreferences(prefName,
+                Activity.MODE_PRIVATE);
+        android.content.SharedPreferences.Editor editor = pref.edit();
+        editor.putString(paramKey, value);
+        editor.commit();
+    }
+
+    /**
+     *
+     * @param paramKey 키값
+     * @param value 가져올 값
+     * @param prefName 파일 이름
+     * @return
+     */
+    public String getValue(String paramKey, String value, String prefName){
+        android.content.SharedPreferences pref = mContext.getSharedPreferences(prefName,
+                Activity.MODE_PRIVATE);
+        try{
+            return pref.getString(paramKey, value);
+        } catch (Exception e){
+            return value;
+        }
+    }
+}
