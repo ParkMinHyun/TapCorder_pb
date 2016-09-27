6c3907ccb4941c678d7be47268f41f943e25efe0
박민현 <algusl92@naver.com>
2016-09-28 03:42:12 +0900

    [민현] BlueTooth Device에 관한 Layout 추가 ( 연결 및 찾기 )

---
+++ TapCorder/app/src/main/java/com/example/osm/appdesign21/Bluetooth_MagicNumber.java
@@ -0,0 +1,27 @@
+package com.example.osm.appdesign21;
+
+/**
+ * Created by BlackMan on 2016-09-28.
+ */
+public class Bluetooth_MagicNumber {
+
+    /* 블루투스에 관한 상수 */
+    private static final boolean D = true;
+
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+
+}
