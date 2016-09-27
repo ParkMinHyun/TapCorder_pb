dfaa6c28b953aea9a7a6013d70e08ea381ab869b
박민현 <algusl92@naver.com>
2016-09-28 03:03:13 +0900

    [민현] 블루투스 통신을 위한 아두이노 연결 및 Serial 통신 완료

--- TapCorder_arduino/TapCorder_arduino.ino
+++ TapCorder_arduino/TapCorder_arduino.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+SoftwareSerial BTSerial(4, 7); //Connect HC-06. Use your (TX, RX) settings
+
+void setup() {
+  
+  Serial.begin(9600);
+  BTSerial.begin(9600);  
+  
+  Serial.println("Start");
+  pinMode(12,INPUT_PULLUP);                                                             
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //int button = digitalRead(12);
+  //Serial.println(button);
+  
+  if (BTSerial.available()) {
+    Serial.write(BTSerial.read());
+    
+  }
+  if (Serial.available()) {
+    BTSerial.write(Serial.read());
+  }
 }
