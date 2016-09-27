7fd3c2c586953454d84992d3de2a08e188982b15
박민현 <algusl92@naver.com>
2016-09-28 04:02:51 +0900

    [민현] 안드로이드 블루투스  송신 기능 추가

--- TapCorder/app/src/main/java/com/example/osm/appdesign21/MainActivity.java
+++ TapCorder/app/src/main/java/com/example/osm/appdesign21/MainActivity.java
@@ -241,6 +241,27 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
             startActivity(discoverableIntent);
         }
     }
+    /**
+     * Sends a message.
+     * @param message  A string of text to send.
+     */
+    private void sendMessage(String message) {
+        // Check that we're actually connected before trying anything
+        if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
+            Toast.makeText(this, R.string.not_connected, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Check that there's actually something to send
+        if (message.length() > 0) {
+            // Get the message bytes and tell the BluetoothChatService to write
+            byte[] send = message.getBytes();
+            mChatService.write(send);
+
+            // Reset out string buffer to zero and clear the edit text field
+            mOutStringBuffer.setLength(0);
+        }
+    }
 
     @Override
     public void onItemClick(int position) {
