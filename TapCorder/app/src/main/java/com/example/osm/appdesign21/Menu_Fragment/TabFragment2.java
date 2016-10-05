3b18010189e5e26d6881d183649bf66a1f302262
박민현 <algusl92@naver.com>
2016-10-06 02:06:40 +0900

    [민현] 가장 가까이에 있는 경찰서 이름 표시 및 전화하기 기능 추가

--- TapCorder/app/src/main/java/com/example/osm/appdesign21/Menu_Fragment/TabFragment2.java
+++ TapCorder/app/src/main/java/com/example/osm/appdesign21/Menu_Fragment/TabFragment2.java
@@ -1,9 +1,11 @@
 package com.example.osm.appdesign21.Menu_Fragment;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -11,6 +13,7 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,7 +51,9 @@ public class TabFragment2 extends Fragment {
     private LatLng mCurrent_Location;
 
     private View inflatedView;
-    public TextView distance;
+    public TextView mDistance;
+    public TextView mPoliceOffice_name;
+    public Button mCall_btn;
     //btn=(Button)inflatedView.findViewById(R.id.)
 
     @Override
@@ -60,17 +65,30 @@ public class TabFragment2 extends Fragment {
         mapView = (MapView) view.findViewById(R.id.gmap);
         mapView.onCreate(savedInstanceState);
 
-        init_Property();
+        init_Property(view);
         init_DB();
         init_Map(view);
 
         return view;
     }
 
-    public void init_Property() {
+    public void init_Property(View view) {
         this.mSpotDbAdapter = new SpotsDbAdapter(this.getActivity());
         this.mGpsTracker = new GPSTracker(this.getContext());
         this.mSpot_array = new ArrayList<>();
+
+        this.mDistance = (TextView)view.findViewById(R.id.between_distance);
+        this.mPoliceOffice_name = (TextView)view.findViewById(R.id.policeStationName);
+        this.mCall_btn = (Button)view.findViewById(R.id.call);
+
+        mCall_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:"+mCall_btn.toString()));
+                startActivity(callIntent);
+            }
+        });
     }
 
     public void init_Map(View view) {
@@ -91,10 +109,10 @@ public class TabFragment2 extends Fragment {
                 .icon(BitmapDescriptorFactory.fromBitmap(resizeMapIcons("userin",140,170))));
 
         String strNumber = String.format("%.1f", CalculationByDistance(short_policeStation,mCurrent_Location)*1000);
-        Toast.makeText(getContext(),String.valueOf( CalculationByDistance(short_policeStation,mCurrent_Location)),Toast.LENGTH_SHORT).show();
 
-        this.distance = (TextView)view.findViewById(R.id.between_distance);
-        distance.setText("약 : " + strNumber + "M ");
+        mDistance.setText("약 : " + strNumber + "M ");
+        mPoliceOffice_name.setText(" 서울 광진 경찰서" + "\n    화양 지구대" );
+        //mPoliceOffice_name.setText(mSpot_array.get(0).get_name());
 
         checkDangerousPermissions();
     }
