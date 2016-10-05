0acc2d2556716ee4724fbcd6aba2817fffebafe5
박민현 <algusl92@naver.com>
2016-10-06 00:55:51 +0900

    [민현] 두 번째 Tab Fragment를 사용하기 위한 초기 설정

--- TapCorder/app/src/main/java/com/example/osm/appdesign21/Menu_Fragment/TabFragment2.java
+++ TapCorder/app/src/main/java/com/example/osm/appdesign21/Menu_Fragment/TabFragment2.java
@@ -6,12 +6,47 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.osm.appdesign21.DB_Excel.SpotData;
+import com.example.osm.appdesign21.DB_Excel.SpotsDbAdapter;
+import com.example.osm.appdesign21.Map.GPSTracker;
 import com.example.osm.appdesign21.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TabFragment2 extends Fragment {
 
+    private GPSTracker mGpsTracker;
+    private SpotsDbAdapter mSpotDbAdapter;
+    private List<SpotData> mSpot_array;
+
+    private MapView mapView;
+    private GoogleMap gMap;
+    private Marker mPoliceMarker;
+    private Marker mPoliceMarker2;
+    private Marker mUserMarker;
+    private LatLng mCurrent_Location;
+
+    private View inflatedView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+
+        View view = inflater.inflate(R.layout.tab_fragment_2, null);
+        mapView = (MapView) view.findViewById(R.id.gmap);
+        mapView.onCreate(savedInstanceState);
+
+
+        init_Property();
+
+        return view;
+    }
+    public void init_Property() {
+        this.mSpotDbAdapter = new SpotsDbAdapter(this.getActivity());
+        this.mGpsTracker = new GPSTracker(this.getContext());
+        this.mSpot_array = new ArrayList<>();
     }
 }
