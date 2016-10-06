a5ac51c8c12b77ee4404bfcd962a4c35720b8cdc
박민현 <algusl92@naver.com>
2016-10-07 02:53:08 +0900

    [민현] Fragment3에 사용자 배터리 % 상태 토스트로 띄움

--- TapCorder/app/src/main/java/com/example/osm/appdesign21/Menu_Fragment/TabFragment3.java
+++ TapCorder/app/src/main/java/com/example/osm/appdesign21/Menu_Fragment/TabFragment3.java
@@ -1,17 +1,48 @@
 package com.example.osm.appdesign21.Menu_Fragment;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.osm.appdesign21.R;
 
 public class TabFragment3 extends Fragment {
 
+    TextView textView;
+
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context ctxt, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            Toast.makeText(getContext(),String.valueOf(level) + "%",Toast.LENGTH_SHORT).show();
+        }
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_3, container, false);
+
+
+        LayoutInflater lf = getActivity().getLayoutInflater();
+        View view = lf.inflate(R.layout.tab_fragment_3, null);
+        textView = (TextView)view.findViewById(R.id.textView);
+
+
+        getActivity().registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        return view;
+
+
     }
+
+
+
+
 }
